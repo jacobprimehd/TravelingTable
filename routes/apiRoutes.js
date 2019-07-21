13e51b93a9721c7007c9d4d0c3606abaf8875878
Jacob Zamano <zamano.jacob@gmail.com>
2019-07-21 13:43:39 -0700

    log in redirects back to homepage

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,9 +4,12 @@ var passport = require("../config/passport");
 
 module.exports = function(app) {
   // Get all examples
-  app.post("/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
+  app.post("/login", passport.authenticate(
+    "local", {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    }
+));
 
   app.post("/register", function(req,res){
     console.log(req.body)
